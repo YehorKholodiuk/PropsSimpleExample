7aea23edfce37fc41eddf431795867f925219d0c
ASUS <Yehor Kholodiuk>
2022-04-30 13:45:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,31 @@ function App() {
     const version = 2;
     const isPublic = true;
     const owner = {
-        name:'PASV',
-        zip:'56566',
-        phone:'65-67-88-555'
+        name: 'PASV',
+        zip: '56566',
+        phone: '65-67-88-555'
 
     }
 
-    const courses = ['JS','React','QA']
+    const courses =
+        [
+            {
+                id: '3444',
+                title: 'react',
+                price: 675,
+            },
+            {
+                id: '34994',
+                title: 'JavaS',
+                price: 475,
+            },
+            {
+                id: '344477',
+                title: 'QA',
+                price: 695,
+            },
+
+        ]
     return (
         <div>
             <Menu siteName={siteName} version={version} isPublic={isPublic} owner={owner} courses={courses}/>
--- src/Menu.js
+++ src/Menu.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 const Menu = (props) => {
     console.log(props)
+    const buyCourse = (id) =>{
+        console.log('buy course',id)
+    }
     return (
         <>
             <div>Site name:{props.siteName}</div>
+
             <div>Version: {props.version}</div>
             <div>Visibility:{props.isPublic? 'Public':'Private'}</div>
             <div>Owner:{props.owner.name} {props.owner.phone}</div>
-            <div>Courses:{props.courses}</div>
+            <div>Courses:</div>
+            <ul>
+                {props.courses.map(el => (
+                    <li key={el.id}>
+                    <strong>{el.title}</strong>, Price:${el.price}
+                        <button onClick={() => buyCourse(el.id)}>Buy</button>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
